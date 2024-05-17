7e55bd4b0e9dac1b9c7b5792dfb3ebd92ddf4714
Osman Topuz <osmantopuz98@gmail.com>
2024-05-17 11:03:12 +0300

    write repository

--- Core/HepsiWebAPI.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/HepsiWebAPI.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace HepsiWebAPI.Application.Interfaces.Repositories
-{
-    public interface IWriteRepository
-    {
+﻿using HepsiWebAPI.Domain.Common;
 
+namespace HepsiWebAPI.Application.Interfaces.Repositories
+{
+    public interface IWriteRepository<T> where T : class, IEntityBase, new()
+    {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IList<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task HardDeleteAsync(T entity);
     }
 }
--- Infrastructure/HepsiWebAPI.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/HepsiWebAPI.Persistence/Repositories/WriteRepository.cs
@@ -1,7 +1,39 @@
-﻿namespace HepsiWebAPI.Persistence.Repositories
-{
-    public class WriteRepository
-    {
+﻿using HepsiWebAPI.Application.Interfaces.Repositories;
+using HepsiWebAPI.Domain.Common;
+using Microsoft.EntityFrameworkCore;
 
+namespace HepsiWebAPI.Persistence.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly DbContext dbContext;
+
+        public WriteRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        private DbSet<T> Table { get => dbContext.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbContext.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await dbContext.AddRangeAsync(entities);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+        }
     }
 }
